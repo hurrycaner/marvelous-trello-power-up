c2c5d6a93c591a278c72d26e6993d66b02f65440
Gustavo Schirmer <hurrycaner@gmail.com>
2019-11-05 01:42:29 -0300

    test 5

--- client.js
+++ client.js
@@ -1,17 +1,24 @@
 /* global TrelloPowerUp */
 
-// var Promise = TrelloPowerUp.Promise;
+var Promise = TrelloPowerUp.Promise;
+
+var getIdBadge = function(t){
+    return t
+        .card('idShort')
+        .get('idShort')
+        .then(function(result){
+            return [{
+                title: 'Card Number', // for detail badges only
+                text: result[1]
+            }];
+        })
+};
 
 TrelloPowerUp.initialize({
     'card-badges': function(t, options){
-        return [{
-            text: t.card('idShort').get('idShort')
-        }];
+        return getIdBadge(t);
     },
     'card-detail-badges': function(t, options) {
-        return [{
-            title: 'Card Number', // for detail badges only
-            text: t.card('idShort').get('idShort')
-        }];
+        return getIdBadge(t);
     }
 });
