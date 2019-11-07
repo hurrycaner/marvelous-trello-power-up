e0bdb504dc47d70b52736cff4d4ff69e91ac6295
Gustavo Schirmer <hurrycaner@gmail.com>
2019-11-06 22:07:30 -0300

    test 7

--- README.md
+++ README.md
@@ -1,32 +1,34 @@
 # Epics
 
+https://developers.trello.com/reference#card-badges
+
 ### Card:
-- card-buttons
-    - Set as epic card
-    - Set as feature card
+- [ ] card-buttons
+    - [ ] Set as epic card
+    - [ ] Set as feature card
 
 ### Epic Card:
-- card-back-section
-    - Listagem de features
-        - Botão: Remover feature (ícone de lixeira, ao lado da feature)
-        - Botão: Linkar feature a um card (ícone de link, ao lado da feature, ao clicar, popup: adicionar um novo card ou linkar a um existente?)
-            - Já linkado: botão remover link (ícone ?)
-    - Botão: Adicionar feature (ícone de +)
-- card-badges
-    - Texto: card magic number
-    - Texto: número de features completas / número total de features
-- card-detail-badges
-    - Texto: card magic number
+- [ ] card-back-section
+    - [ ] Listagem de features
+        - [ ] Botão: Remover feature (ícone de lixeira, ao lado da feature)
+        - [ ] Botão: Linkar feature a um card (ícone de link, ao lado da feature, ao clicar, popup: adicionar um novo card ou linkar a um existente?)
+            - [ ] Já linkado: botão remover link (ícone ?)
+    - [ ] Botão: Adicionar feature (ícone de +)
+- [ ] card-badges
+    - [ ] Texto: card magic number
+    - [ ] Texto: número de features completas / número total de features
+- [ ] card-detail-badges
+    - [ ] Texto: card magic number
 
 ### Feature Card:
-- card-back-section
-    - Botão: ir para o Epic
-    - Checkbox: Finalizar a feature
-    - Texto: Nome da feature
-    - Botão: Remover link para a feature
-- card-badges
-    - Texto: card magic number
-    - Botão: Feature name (verde completa / vermelha incompleta)
-- card-detail-badges
-    - Texto: card magic number
+- [ ] card-back-section
+    - [ ] Botão: ir para o Epic
+    - [ ] Checkbox: Finalizar a feature
+    - [ ] Texto: Nome da feature
+    - [ ] Botão: Remover link para a feature
+- [ ] card-badges
+    - [ ] Texto: card magic number
+    - [ ] Botão: Feature name (verde completa / vermelha incompleta)
+- [ ] card-detail-badges
+    - [ ] Texto: card magic number
  
--- client.js
+++ client.js
@@ -1,12 +1,14 @@
 /* global TrelloPowerUp */
 
+// https://developers.trello.com/reference#card-badges
+
 var Promise = TrelloPowerUp.Promise;
 
-var getIdBadge = function(t){
+var getIdBadge = function (t) {
     return t
         .card('idShort')
         .get('idShort')
-        .then(function(result){
+        .then(function (result) {
             return [{
                 title: 'Card Number',
                 text: '#' + result
@@ -14,11 +16,123 @@ var getIdBadge = function(t){
         })
 };
 
+// var getCardButtons = function (t) {
+//     return t
+//             .get('card', 'shared', 'epic', [])
+//             .then(function (result) {
+//                 if (result.length === 0) {
+//                     //
+//                 }
+//
+//                 let done = 0;
+//                 let total = 0;
+//
+//                 for (let i = 0; i < result.length; i++) {
+//                     if (result[i] && result[i].name) {
+//                         if (done) done++;
+//                         total++;
+//                     }
+//                     if (total > 0) {
+//                         return [{
+//                             // its best to use static badges unless you need your badges
+//                             // to refresh you can mix and match between static and dynamic
+//                             title: 'Bla',
+//                             text: done + '/' + total,
+//                             color: null
+//                         }]
+//                     }
+//
+//                     // var feat = {
+//                     //     name: "",
+//                     //     done: false,
+//                     //     cardNum: "",
+//                     //     cardId: ""
+//                     // }
+//                 }
+//                 ret.push({
+//                     // usually you will provide a callback function to be run on button click
+//                     // we recommend that you use a popup on click generally
+//                     icon: GRAY_ICON, // don't use a colored icon here
+//                     text: 'Add feature',
+//                     callback: onBtnClick,
+//                     condition: 'edit'
+//                 });
+//             } else {
+//                 // is not epic
+//                 ret.push({
+//                     // usually you will provide a callback function to be run on button click
+//                     // we recommend that you use a popup on click generally
+//                     icon: GRAY_ICON, // don't use a colored icon here
+//                     text: 'Open Popup',
+//                     callback: onBtnClick,
+//                     condition: 'edit'
+//                 });
+//             }
+//             if (result[1]) {
+//                 // is feature
+//                 ret.push({
+//                     // usually you will provide a callback function to be run on button click
+//                     // we recommend that you use a popup on click generally
+//                     icon: GRAY_ICON, // don't use a colored icon here
+//                     text: 'Open Popup',
+//                     callback: onBtnClick,
+//                     condition: 'edit'
+//                 });
+//             } else {
+//                 // is not feature
+//                 ret.push({
+//                     // usually you will provide a callback function to be run on button click
+//                     // we recommend that you use a popup on click generally
+//                     icon: GRAY_ICON, // don't use a colored icon here
+//                     text: 'Open Popup',
+//                     callback: onBtnClick,
+//                     condition: 'edit'
+//                 });
+//             }
+//             return ret;
+//         });
+// };
+
+var onBtnClick = function (t, opts) {
+    console.log('Someone clicked the button');
+    console.log('t: ', t);
+    console.log('opts: ', opts);
+};
+
 TrelloPowerUp.initialize({
-    'card-badges': function(t, options){
+    'card-buttons': function (t, options) {
+        return t
+            .get('card', 'shared', 'epic', [])
+            .then(function (result) {
+                var ret = [{
+                    // icon: GRAY_ICON,
+                    text: 'Link to feature',
+                    callback: onBtnClick,
+                    condition: 'edit'
+                }];
+                if (result.length === 0) {
+                    ret.unshift({
+                        // icon: GRAY_ICON,
+                        text: 'Show Epic',
+                        callback: onBtnClick,
+                        condition: 'always'
+                    })
+                } else {
+                    ret.unshift({
+                        // icon: GRAY_ICON,
+                        text: 'Hide Epic',
+                        callback: onBtnClick,
+                        condition: 'always'
+                    })
+                }
+
+                return ret
+            })
+    },
+    'card-badges': function (t, options) {
         return getIdBadge(t);
     },
-    'card-detail-badges': function(t, options) {
+    'card-detail-badges': function (t, options) {
         return getIdBadge(t);
     }
 });
